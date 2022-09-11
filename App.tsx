dc728606ede83b3502eb997c4cc1c893da7f7a38
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-09-11 15:46:35 -0300

    template

--- App.tsx
+++ App.tsx
@@ -4,35 +4,13 @@ import {
   Text,
   View,
 } from 'react-native';
-import Main from './src/components/Main';
-import { styles as imStyles } from './src/components/Form';
 
-export const styles = StyleSheet.create({
-  container: {
-    fontSize: '50px',
-    flex: 0.9,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '80%',
-    alignSelf: 'center',
-    height: '100%',
-  },
-  main: {
-    flex: 1,
-    backgroundColor: '#e6e6e6',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export const styles = StyleSheet.create({});
 
 export default function App() {
   return (
-    <View style={styles.main}>
-      <Text style={imStyles.title}>onebithealth</Text>
-      <View style={styles.container}>
-        <Main />
-      </View>
+    <View>
+      <Text>App</Text>
     </View>
   );
 }
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,17 +1,8 @@
-import App from './components/Main';
+import App from '../App';
 import { render, screen, fireEvent } from './test-utils';
 
 describe('App', () => {
-  it('render two text inputs where you can type and calculate your body mass index', () => {
-    const s = render(<App />).toJSON();
-    const heightInput = screen.getByPlaceholderText('ex: 1.72');
-    const weightInput = screen.getByPlaceholderText('ex: 60');
-
-    fireEvent.changeText(heightInput, '1.72');
-    fireEvent.changeText(weightInput, '60');
-
-    const calculateBtn = screen.getByRole('button');
-    fireEvent.press(calculateBtn);
-    expect(s).toMatchSnapshot();
+  it('', () => {
+    //
   });
 });
