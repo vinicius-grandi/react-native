a453bbeed0fb2c47ffe5a247aa1ddbc2b699ed6f
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-09-12 18:39:52 -0300

    styling

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,11 @@
 import { useRef, useState } from 'react';
-import { ScrollView as View, TextInput } from 'react-native';
+import { ScrollView as View, TextInput, Text } from 'react-native';
 import List from './List';
 import Buttons from './Buttons';
 
-export type Task = { id: number, name: string };
+export type Task = { id: number; name: string };
 export type CompletedTasks = {
-  [key in number]: null | true
+  [key in number]: null | true;
 };
 
 export default function Main() {
@@ -13,6 +13,7 @@ export default function Main() {
   const [newTask, setNewTask] = useState('');
   const [selectedTask, setSelectedTask] = useState<number | null>(null);
   const [completedTasks, setCompletedTasks] = useState<CompletedTasks>({});
+  const input = useRef<TextInput>(null);
 
   const id = useRef(0);
 
@@ -54,37 +55,65 @@ export default function Main() {
     if (directions === 'up') {
       if (currIdx - 1 === -1) return;
 
-      [arrClone[(currIdx - 1)], arrClone[currIdx]] = [arrClone[currIdx], arrClone[currIdx - 1]];
+      [arrClone[currIdx - 1], arrClone[currIdx]] = [
+        arrClone[currIdx],
+        arrClone[currIdx - 1],
+      ];
     } else {
       if (currIdx + 1 > tasks.length - 1) return;
 
-      [arrClone[currIdx + 1], arrClone[currIdx]] = [arrClone[currIdx], arrClone[currIdx + 1]];
+      [arrClone[currIdx + 1], arrClone[currIdx]] = [
+        arrClone[currIdx],
+        arrClone[currIdx + 1],
+      ];
     }
 
     setTasks(arrClone);
   };
 
   return (
-    <View>
-      <List
-        tasks={tasks}
-        setSelectedTask={setSelectedTask}
-        selectedId={selectedTask}
-        handleCompletedTasks={handleCompletedTasks}
-        completedTasks={completedTasks}
-      />
-      <TextInput
-        placeholder="ex: buy coffee"
-        value={newTask}
-        onChangeText={(t) => setNewTask(t)}
-      />
-      <Buttons
-        setTasks={setTasks}
-        handleTask={handleNewTask}
-        removeAllCompleted={removeAllCompleted}
-        handleSelectedTask={handleSelectedTask}
-        moveUpOrDown={moveUpOrDown}
-      />
+    <View keyboardShouldPersistTaps="handled">
+      <Text style={{ fontSize: 32, color: '#f02d2d', textAlign: 'center' }}>
+        TODO LIST
+      </Text>
+      <View
+        style={{ height: 100, backgroundColor: '#e7e7e7' }}
+        persistentScrollbar
+      >
+        <List
+          tasks={tasks}
+          setSelectedTask={setSelectedTask}
+          selectedId={selectedTask}
+          handleCompletedTasks={handleCompletedTasks}
+          completedTasks={completedTasks}
+        />
+      </View>
+      <View keyboardShouldPersistTaps="never">
+        <Text style={{ marginTop: 10 }}>TASK</Text>
+        <TextInput
+          placeholder="ex: buy coffee"
+          value={newTask}
+          onChangeText={(t) => setNewTask(t)}
+          ref={input}
+          onPressOut={() => input.current?.focus()}
+          style={{
+            borderStyle: 'solid',
+            borderColor: 'black',
+            borderWidth: 1,
+            padding: 10,
+            borderRadius: 3,
+          }}
+        />
+      </View>
+      <View keyboardShouldPersistTaps="always">
+        <Buttons
+          setTasks={setTasks}
+          handleTask={handleNewTask}
+          removeAllCompleted={removeAllCompleted}
+          handleSelectedTask={handleSelectedTask}
+          moveUpOrDown={moveUpOrDown}
+        />
+      </View>
     </View>
   );
 }
